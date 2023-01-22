b48b8c392d5e2108f038c70ae2bf7174c8ef45c3
Nitishkumarnandi123 <nitishkumarnandi145@gmail.com>
2023-01-22 22:36:22 +0530

    copy the thapa project

---
+++ src/index.js
@@ -0,0 +1,50 @@
+/*import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+    /*<div><h1>Nitish kumar nandi</h1>
+    <h3><ol>
+     <li><p>my name is manjulata kundu</p></li>
+     <li><p>my name is manjulata kundu</p></li>
+     <li><p>my name is manjulata kundu</p></li></ol></h3>
+     </div>,
+     document.getElementById("root")); */
+    /*import React from "react";
+    import ReactDOM  from "react-dom";
+    const fname="nitish";
+    const lname="nandi";
+    ReactDOM.render(<>
+    <h1>MY name is {fname} {lname}</h1>
+   <ol><li><p>at-narasingha pur</p></li>
+   <li><p>at-narasingha pur</p></li>
+   <li><p>at-narasingha pur</p></li></ol></>,
+    document.getElementById("root"));*/
+   /* import React from "react";
+    import ReactDom from "react-dom";
+    const fname= "Nitish";
+    const mname= "kumar";
+    const lname= "Nandi";
+    ReactDom.render(//<div><h1>{fname +""+mname+} </h1>
+    //<p>at-narasingha pur</p>
+    // </div>,
+    <div>
+        <h1>{`my name is ${fname} $ {mname} $ {lname}`}</h1>
+       <p> at-narasingha at-narasingha</p> 
+    </div>,
+    document.getElementById("root"));*/
+    /*import React from "react";
+    import ReactDom from "react-dom";
+    const currentDate=new Date();
+    const name="Nitish kumar nandi";
+    ReactDom.render(<>
+    <h1>
+        HELLO MY NAME IS:{name}
+    </h1>
+    <p><h3>currentDate is={currentDate}</h3></p>
+    </>,
+    document.getElementById("root"));*/
+    import React from "react";
+    import ReactDom from "react-dom/client";
+    const myElement=<h1>hello nitish kumar nandi</h1>
+    const root= ReactDom.createRoot(document.getElementById("root"));
+    root.render(myElement);
+
